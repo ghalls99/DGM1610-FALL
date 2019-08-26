4b6d4bbb44948f3aceacb7553a962ad0c8d63871
Brytan <brytanpaigeh@gmail.com>
2019-08-26 17:17:21 -0600

    Added Variables

---
+++ New Unity Project/Assets/scripts/Assignments/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string nyame="Brytan";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log(nyame); 
+    }
+
+   
+}
