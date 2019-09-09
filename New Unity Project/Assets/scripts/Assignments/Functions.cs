41fb6356c714b5f3b2849582231a01ed2b57f49d
Brytan <brytanpaigeh@gmail.com>
2019-09-09 17:06:25 -0600

    Added prototype 1

---
+++ New Unity Project/Assets/scripts/Assignments/Functions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    // string firstname;
+    // string lastname;
+    // int age;
+
+    int num1;
+    int num2;
+    int total;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        // firstname = "brytan";
+
+        // lastname = "hagerman";
+
+        // age = "18";
+
+        // Customer (firstname, lastname, age); 
+        CalAdd(11,23);
+    }
+
+    int CalAdd (int n1, int n2)
+    {
+        Debug.Log("Number 1 = " + n1 + "Number 2 =" + n2);
+        total = n1 + n2;
+        
+
+        return total;
+        Debug.Log(total);
+    }
+
+    // void Customer (string fname, string lname, int a)
+
+    //     Debug.Log(fname + " " + lname + " " + a);
+    void Update ()
+    {
+
+    }
+}
--- New Unity Project/Assets/scripts/Assignments/Variables.cs
+++ New Unity Project/Assets/scripts/Assignments/Variables.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    string nyame="Brytan";
+    string names="Brytan";
     int age = 18;
     float height = 5.4f;
     bool married = false;
@@ -17,10 +17,10 @@ public class Variables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("first name; " + (nyame); 
-       Debug.Log("age; " + age);
-       Debug.Log("heaight; " + height);
-       Debug.Log("married; " + married);
+       Debug.Log("first name " + names);
+       Debug.Log("age " + age);
+       Debug.Log("height " + height);
+       Debug.Log("married " + married);
     }
 
    
