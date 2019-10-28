54951b3acb31133cc8b3517cb0514975641dbf5a
Brytan <brytanpaigeh@gmail.com>
2019-10-28 16:57:20 -0600

    Running animation

--- Create with code/prototype 3/Assets/Scipts/PlayerController.cs
+++ Create with code/prototype 3/Assets/Scipts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    private Rigidbody playerRb;
+   private Animator playerAnim;
    public float jumpForce = 10;
    public float gravityModifier;
    public bool isOnGround = true;
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
     }
